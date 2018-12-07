ca788442dd0313114eced1286540b1b33f5bc76b
Alon Hen Krokhmal <alohen@gmail.com>
2018-12-07 23:45:09 +0200

    continued implementation of general keys functions added: EXPIRE PEXPIRE EXPIREAT PEXPIREAT TTL PTTL PERSIST KEYS

--- expiration.go
+++ expiration.go
@@ -5,7 +5,10 @@ import "time"
 type Expirable interface{
 	IsExpired() bool
 	TTL() int64
+	PTTL() int64
 	SetTTL(time.Duration)
+	SetExpirationTime(time.Time)
+	RemoveTTL()
 }
 
 type expirationHandler struct {
@@ -15,24 +18,36 @@ type expirationHandler struct {
 // Checks whether the value is expired
 func (this *expirationHandler) IsExpired() bool {
 	// if the expiration time wasn't set, they key can't be expired
-	if value.ExpirationTime.IsZero() {
+	if this.ExpirationTime.IsZero() {
 		return false
 	}
 
-	if time.Now().After(value.ExpirationTime) {
+	if time.Now().After(this.ExpirationTime) {
 		return true
 	}
 
 	return false
 }
 
-// TTL return the amount of milliseconds left until expiration time
-func (this *expirationHandler) TTL() int64 {
+// GetTTLInSeconds return the amount of milliseconds left until expiration time
+func (this *expirationHandler) GetTTLInSeconds() int64 {
 	timeLeft := time.Until(this.ExpirationTime)
-	millisecondsLeft := timeLeft.Nanoseconds() / 1000000
-	return millisecondsLeft
+	return int64(timeLeft.Seconds())
+}
+
+func (this *expirationHandler) GetTTLInMilliseconds() int64 {
+	timeLeft := time.Until(this.ExpirationTime)
+	return timeLeft.Nanoseconds() / 1000000
 }
 
 func (this *expirationHandler) SetTTL(duration time.Duration) {
 	this.ExpirationTime = time.Now().Add(duration)
+}
+
+func (this *expirationHandler) SetExpirationTime(expirationTime time.Time) {
+	this.ExpirationTime = expirationTime
+}
+
+func (this *expirationHandler) RemoveTTL() {
+	this.ExpirationTime = time.Time{}
 }
--- redis_db.go
+++ redis_db.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"sync"
 	"time"
+	"path"
+	"path/filepath"
+	"github.com/gobwas/glob"
 )
 
 // Key Type enumeration
@@ -17,13 +20,13 @@ const (
 	SortedSetKeyType KeyType = 4
 )
 
-
 type RedisDB struct {
 	StringKeys    map[string]*RedisStringValue
 	ListKeys      map[string]*RedisListValue
 	HashKeys      map[string]*RedisHashValue
 	SetKeys       map[string]*RedisSetValue
 	SortedSetKeys map[string]*RedisSortedSetValue
+	keysToTypes   map[string]KeyType
 	lock          sync.Mutex
 }
 
@@ -44,14 +47,84 @@ func (db *RedisDB) EXISTS(key string) bool {
 	return db.locateKey(key) != -1
 }
 
-func (db *RedisDB) EXPIRE(key string, seconds int) error {
-	return db.setKeyExpiration(key, time.Duration(seconds) * time.Second)
+func (db *RedisDB) EXPIRE(key string, seconds int) int64 {
+	if err := db.setTTL(key, time.Duration(seconds)*time.Second); err != nil {
+		return -1
+	}
+	return 0
+}
+
+func (db *RedisDB) EXPIREAT(key string, timestamp int64) int64 {
+	expirationTime := time.Unix(timestamp,0)
+	if err := db.setExpirationTime(key, expirationTime) ;err != nil {
+		return -1
+	}
+	return 0
+}
+
+func (db *RedisDB) PEXPIRE(key string, milliseconds int) int64 {
+	if err := db.setTTL(key, time.Duration(milliseconds)*time.Millisecond); err != nil {
+		return -1
+	}
+
+	return 0
+}
+
+func (db *RedisDB) PEAPIREAT(key string, timestamp int64) int64 {
+	expirationTime := time.Unix(0,timestamp * 1000000)
+	if err := db.setExpirationTime(key, expirationTime) ;err != nil {
+		return -1
+	}
+	return 0
+}
+
+func (db *RedisDB) KEYS(globString string) []string {
+	var matchedKeys []string
+	searchGlob := glob.MustCompile(globString)
+	for key := range db.keysToTypes {
+		if searchGlob.Match(key) {
+			matchedKeys = append(matchedKeys, key)
+		}
+	}
+
+	return matchedKeys
+}
+
+func (db *RedisDB) TTL(key string) int64 {
+	keyType := db.locateKey(key)
+	if keyType == -1 {
+		return -1
+	}
+
+	value := db.getValueAsExpirable(key, keyType)
+	return value.TTL()
+}
+
+func (db *RedisDB) PTTL(key string) int64 {
+	keyType := db.locateKey(key)
+	if keyType == -1 {
+		return -1
+	}
+
+	value := db.getValueAsExpirable(key, keyType)
+	return value.PTTL()
+}
+
+func (db *RedisDB) PERSIST(key string) int64 {
+	keyType := db.locateKey(key)
+	if keyType == -1 {
+		return 0
+	}
+
+	value := db.getValueAsExpirable(key,keyType)
+	value.RemoveTTL()
+
+	return 1
 }
 
 
-
 // utility functions
-func (db *RedisDB) setKeyExpiration(key string, duration time.Duration) error {
+func (db *RedisDB) setTTL(key string, duration time.Duration) error {
 	keyType := db.locateKey(key)
 	if keyType == -1 {
 		return fmt.Errorf("missing key")
@@ -62,6 +135,21 @@ func (db *RedisDB) setKeyExpiration(key string, duration time.Duration) error {
 		return fmt.Errorf("missing key")
 	}
 	value.SetTTL(duration)
+
+	return nil
+}
+
+func (db *RedisDB) setExpirationTime(key string, duration time.Time) error {
+	keyType := db.locateKey(key)
+	if keyType == -1 {
+		return fmt.Errorf("missing key")
+	}
+
+	value := db.getValueAsExpirable(key, keyType)
+	if value == nil {
+		return fmt.Errorf("missing key")
+	}
+	value.SetExpirationTime(duration)
 
 	return nil
 }
@@ -99,26 +187,12 @@ func (db *RedisDB) deleteKeyByType(key string, keyType KeyType) {
 }
 
 func (db *RedisDB) locateKey(key string) (KeyType) {
-	var exists bool
-	var keyType KeyType = -1
-
-	if _, exists = db.StringKeys[key]; exists {
-		keyType = StringKeyType
-	}
-	if _, exists = db.ListKeys[key]; exists {
-		keyType =  ListKeyType
-	}
-	if _, exists = db.HashKeys[key]; exists {
-		keyType = HashKeyType
-	}
-	if _, exists = db.SetKeys[key]; exists {
-		keyType =  SetKeyType
-	}
-	if _, exists = db.SortedSetKeys[key]; exists {
-		keyType = SortedSetKeyType
+	keyType, exists := db.keysToTypes[key]
+	if !exists {
+		return -1
 	}
 
-	if db.deleteKeyIfExpired(key,keyType) {
+	if db.deleteKeyIfExpired(key, keyType) {
 		return -1
 	}
 
@@ -126,13 +200,13 @@ func (db *RedisDB) locateKey(key string) (KeyType) {
 }
 
 func (db *RedisDB) deleteKeyIfExpired(key string, keyType KeyType) bool {
-	value := db.getValueAsExpirable(key,keyType)
+	value := db.getValueAsExpirable(key, keyType)
 	if value == nil {
 		return false
 	}
 
 	if value.IsExpired() {
-		db.deleteKeyByType(key,keyType)
+		db.deleteKeyByType(key, keyType)
 		return true
 	}
 
